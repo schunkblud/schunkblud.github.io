8e14f8940b5ad05beb32b6c7ba7e7cf8d83a4135
schunkblud <ofarukaradag@gmail.com>
2026-01-09 02:36:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-fetch("data.json")
-  .then(response => response.json())
-  .then(data => {
-    const grid = document.getElementById("animeGrid");
-
-    data.forEach(anime => {
-      const card = document.createElement("div");
-      card.className = "anime-card";
-
-      card.innerHTML = `
-        <img src="${anime.image}">
-        <span class="ep">${anime.episode}</span>
-        <span class="lang">${anime.lang}</span>
-        <h3>${anime.title}</h3>
-      `;
-
-      grid.appendChild(card);
-    });
-  })
-  .catch(err => console.error("Anime yüklenemedi:", err));
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("active");
+}
