a4cd66d0244ab6d74b5ee9bddd34d74365773ca5
schunkblud <ofarukaradag@gmail.com>
2026-01-09 02:17:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// İleride filtre, arama, anime ekleme burada olacak
+fetch("data.json")
+  .then(response => response.json())
+  .then(data => {
+    const grid = document.getElementById("animeGrid");
+
+    data.forEach(anime => {
+      const card = document.createElement("div");
+      card.className = "anime-card";
+
+      card.innerHTML = `
+        <img src="${anime.image}">
+        <span class="ep">${anime.episode}</span>
+        <span class="lang">${anime.lang}</span>
+        <h3>${anime.title}</h3>
+      `;
+
+      grid.appendChild(card);
+    });
+  })
+  .catch(err => console.error("Anime yüklenemedi:", err));
